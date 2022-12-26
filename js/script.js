938135ecc5b6196140ceac0da4d89d957a87a38a
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-12-25 23:46:15 -0300

    :package: script criptografia

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var input = document.querySelector("#input-texto");
+var outinput = document.querySelector("#output");
+
+function criptografar() {
+  var texto = input.ariaValueMax;
+  var resultado = texto.replace;
+}
