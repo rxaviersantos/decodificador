f496409367fa6373b30b19dada7bcb406174e1e1
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-12-27 16:24:18 -0300

    :package: função criptografia

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ var input = document.querySelector("#input-texto");
 var outinput = document.querySelector("#output");
 
 function criptografar() {
-  var texto = input.ariaValueMax;
-  var resultado = texto.replace;
+  var texto = input.value;
+
+  var resultado = texto
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+
+  document.getElementById("output").innerHTML =
+    '<textera readonly id="input-texto">' +
+    resultado +
+    "<textera>" +
+    '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
 }
